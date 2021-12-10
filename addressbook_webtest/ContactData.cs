93424a45accfea98502c42c2e3bf65d4f7620998
Надя Морозова <nadushkaspb@mail.ru>
2021-12-10 16:42:07 +0300

    4 Задание первого урока

---
+++ addressbook_webtest/ContactData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook
+{
+    class Contact
+    {
+        private string lastname;
+        private string firstname;
+
+        public Contact(string lastname, string firstname)
+        {
+            this.lastname = lastname;
+            this.firstname = firstname;
+        }
+
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+
+        }
+    }
+}
